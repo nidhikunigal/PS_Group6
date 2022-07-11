c2fc54ceebebe7251a0620b12ac8c7272f12f3ca
Sammy Chen <sammychen6@Sammys-MacBook-Pro.local>
2022-07-11 09:57:44 -0400

    updated file

--- jsonparse.py
+++ jsonparse.py
@@ -1,4 +1,5 @@
 import json
+from weakref import ref
 
 def fileHandle(file, vehicle):
     with open("allDataCSV.json", "r") as data_file:
@@ -24,10 +25,40 @@ def fileHandle(file, vehicle):
         specific_vehicle.append(temp)
     return specific_vehicle
 
+def fordfilter(input):
+
+    returnVal = []
+    for i in input:
+        for j in i:
+            value = j.split(" ")
+            yearVal = value[0]
+            makeVal = value[1]
+            modelVal = value[2]
+            trimVal = value[3]
+            returnVal.append("{Year:" + yearVal + ", Make:" + makeVal + ", Model:" + modelVal + ", Trim:" + trimVal + "}")
+    
+    return(returnVal)
+
+def parseSilverado(input):
+    #combine maybe????????
+    arr = []
+    year = input[1]
+    make = input[2]
+    model = input[3] + " " + input[4]
+    trim = input[5]
+
+    newVal = '{"year": "' + year + '", "make": "' + make + \
+        '", "model": "' + model + '", "trim": "' + trim + '"}'
+    arr.append(newVal)
+    return arr
+
 def main():
     file = "allDataCSV.json"
-    vehicle = "Toyota"
-    print(fileHandle(file, vehicle))
+    vehicle = "Ford"
+    refined_data = fileHandle(file, vehicle)
+    res = fordfilter(refined_data)
+    #print(res)
+    print(refined_data)
 
 if __name__ == "__main__":
   main()
