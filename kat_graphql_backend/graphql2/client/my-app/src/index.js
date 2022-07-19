240c977249349d9fba333b074d4a6ddd4a6aa944
Sammy Chen <samchen6@publicisgroupe.net>
2022-07-19 10:54:55 -0400

    data display

--- kat_graphql_backend/graphql2/client/my-app/src/index.js
+++ kat_graphql_backend/graphql2/client/my-app/src/index.js
@@ -3,32 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  cache: new InMemoryCache(),
-});
-
-client
-  .query({
-    query: gql`
-      query ExampleQuery($type: String!) {
-        partByType(Type: $type) {
-          Product_Name
-          Company
-          Cost
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
