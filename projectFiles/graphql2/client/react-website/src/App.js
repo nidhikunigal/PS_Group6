771c683f17c6f4fa33cc0b6d7ee085e2046eae0a
Casey Oppenheim <casoppen@publicisgroupe.net>
2022-07-25 10:23:45 -0400

    updated front end in react-website!

--- projectFiles/graphql2/client/react-website/src/App.js
+++ projectFiles/graphql2/client/react-website/src/App.js
@@ -9,8 +9,12 @@ import Blogs from './pages/blogs';
 import SignUp from './pages/signup';
 import Contact from './pages/contact';
 import Results from './pages/results';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 
 function App() {
+	const client = new ApolloClient({ cache: new InMemoryCache(), uri: "http://localhost:4000/graphql" });
+
 return (
 	<Router>
 	<Navbar />
