58c155734399f22957bf29013620ea0c50b36937
Casey Oppenheim <casoppen@publicisgroupe.net>
2022-07-27 11:40:19 -0400

    hopefully everything is fine now- no one fuck w this at all plz

--- projectFiles/graphql2/client/react-website/src/pages/results.js
+++ projectFiles/graphql2/client/react-website/src/pages/results.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { RefineHead, RefineResults, ChangeVeh, Silly, ResultHeader, ResultsPage, Column1, Column2, ResultGrid, GridEl } from "./resultStyle";
+import { RefineHead, RefineResults, ChangeVeh, Silly, ResultHeader, ResultsPage, Column1, Column2, ResultGrid, gridel } from "./resultStyle";
 import { useNavigate } from "react-router-dom";
 import littleCar from "./currveh.jfif";
 import {self} from "./index";
@@ -8,7 +8,7 @@ import { useQuery, useLazyQuery, gql } from "@apollo/client";
 import tire from "./tire.jpg";
 import bumper from "./tempBumper.jpg";
 import levelingKit from "./levelingKit.jpg";
-
+ 
 const QUERY_YEAR_MAKE_MODEL = gql`
 query yearMakeModel($year: String, $make: String, $model: String) {
   partByYear(Year: $year, Make: $make, Model: $model) {
@@ -29,63 +29,30 @@ query yearMakeModel($year: String, $make: String, $model: String) {
   }
 }
 `;
-
+ 
 var unique = [];
 function ResultGridFun(data, error, loading){
     let j = "";
     if(!error && !loading){
         unique = [... new Map(data.partByYear.map(item => [item.Product_Name, item])).values()];
         console.log(data);
-        console.log(unique); 
+        console.log(unique);
         console.log(self.Part);
         if(self.Part != "All Part" && self.Part != ""){
             refineUnique(unique);
         }
         for(let i = 0; i <unique.length; i++){
             if(unique[i] != null){
-                // var value = unique[i];
-                // var img = document.createElement('img');
-                // switch(value.VehicleParts[0].Type){
-                //     case 'Bumper':
-                //         //img = ' <img src={bumper} />';
-                //         img.setAttribute('src', bumper);
-                //         img.setAttribute('width', '50px');
-                //         break;
-                //     case 'Suspension':
-                //         img.setAttribute('src', bumper);
-                //         img.setAttribute('width', '50px');
-                //         //display = display.concat(" <img src={bumper}/>");
-                //         break;
-                //     case 'Leveling':
-                //         img.setAttribute('src', levelingKit);
-                //         img.setAttribute('width', '50px');
-                //         //display = display.concat(" <img src={levelingKit}/>");
-                //         break;
-                //     case 'Wheel':
-                //         img.setAttribute('src', tire);
-                //         img.setAttribute('width', '50px');
-                //         //display = display.concat(" <img src={tire}/>");
-                //         break;
-                //     case 'Fenders':
-                //         img.setAttribute('src', bumper);
-                //         img.setAttribute('width', '50px');
-                //         //display = display.concat(" <img src={bumper}/>");
-                //}
-                j+="<div class=grid-item>" + unique[i].Product_Name + internalGrid(unique[i].VehicleParts[0].Type)+ "</div> ";
+                j+="<div class=grid-item><style type=text/css> .grid-item{display: flex; flex-direction:column; align-items: center; justify-content: flex-start;  text-align: center; background-color: pink;}</style>" + internalGrid(unique[i].VehicleParts[0].Type) + "<a style=\"color:blue; font-weight: 600; font-family:Sans-serif; font-size:1em; \" activeStyle>" + unique[i].Product_Name + "</a> <p id=price><style type=text/css> #price{position:relative;  bottom: 0; left: 0; }</style>" +unique[i].VehicleParts[0].Cost + "</p> </div> ";
             }
         }
-        //console.log(j);
-        //const currHTML = document.getElementsByTagName('ResultGrid')[0].innerHTML;
-       // const nextHTML = currHTML + j; 
-        //document.getElementsByTagName('ResultGrid')[0].innerHTML = j;
-       
     }
     return(
         <ResultGrid dangerouslySetInnerHTML={{__html: j}}>
         </ResultGrid>
     )
 }
-
+ 
 function refineUnique(data){
     for(let i = 0; i < data.length; i++){
         if(data[i].VehicleParts[0].Type != self.Part){
@@ -93,11 +60,11 @@ function refineUnique(data){
         }
     }
 }
-
+ 
 function internalGrid(type){
     var SRC = null;
     //var img = document.createElement("img");
-
+ 
         switch(type){
             case 'Bumper':
                 SRC = bumper;
@@ -114,46 +81,42 @@ function internalGrid(type){
             case 'Fenders':
                 SRC = bumper;
         }
-        // if(document.getElementById(id) != null){
-        //     document.getElementById(id).appendChild(img);
-        // }
-        //console.log(document.getElementById(i));
-        return ("<img src=" + SRC + " width=100% />");
+        return ("<img src=" + SRC + " width=250px />");
 }
-
-    
-
-
+ 
+   
+ 
+ 
 function PDPage(){
-	let nav = useNavigate();
-	const routeChange = () =>{
-		let path = '/details';
-		nav(path);
-
-	}
-
-	return (
+    let nav = useNavigate();
+    const routeChange = () =>{
+        let path = '/details';
+        nav(path);
+ 
+    }
+ 
+    return (
         <div></div>
-	)
+    )
 }
-
+ 
 function ChangeVehFun(){
     let nav = useNavigate();
-	const routeChange = () =>{
-		let path = '/';
-		nav(path);
-
-	}
-
-	return (
-		<button color="primary" className="px-4"onClick={routeChange}
+    const routeChange = () =>{
+        let path = '/';
+        nav(path);
+ 
+    }
+ 
+    return (
+        <button color="primary" className="px-4"onClick={routeChange}
               >
               Change Vehicle
             </button>
-	)
+    )
    
 }
-
+ 
 const Results = () => {
     //const [loadData, { data: yearData }] = useLazyQuery(QUERY_YEAR_MAKE_MODEL);
     const{data, error, loading} = useQuery(QUERY_YEAR_MAKE_MODEL, {variables: { year: self.Year,
@@ -168,15 +131,15 @@ const Results = () => {
     //console.log(data.partByYear.length);
     //var size = data.partByYear.length;
 return (
-	<ResultsPage >
+    <ResultsPage >
         <Column1>
-            <Silly> 
+            <Silly>
                 <img src={littleCar}  height="60px"/>
                 Current Vehicle: {self.Year} {self.Make} {self.Model}
             </Silly>
-            
+           
             <ChangeVehFun />
-                
+               
             <RefineHead> Refine By:</RefineHead>
             <RefineResults>
                 <select id="Brand">
@@ -190,29 +153,29 @@ return (
                     <option value="none">List rest of products</option>
                 </select>
                 <button id="refine" onClick="refine()">Refine</button>
-
+ 
             </RefineResults>
              
-
+ 
         </Column1>
         <Column2>
             <ResultHeader>
                 Showing {self.Part}'s for {self.Year} {self.Make} {self.Model}
-                
-        
+               
+       
             </ResultHeader>
-            
+           
             <div id="grid-container" class="grid">
                 <ResultGrid>
                     {ResultGridFun(data, error, loading) }
                 </ResultGrid>
-                {/* {internalGrid(unique)} */}
             </div>
             <div id="grid-container" class="grid">
             </div>
         </Column2>
-	</ResultsPage>
+    </ResultsPage>
 );
 };
-
+ 
 export default Results;
+
